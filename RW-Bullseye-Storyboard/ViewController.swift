63e915e291206594cebff53aa1b892c918dc608a
Chris Price <christopher.r.price@gmail.com>
2020-12-05 20:41:23 -0500

    Added a currentValue variable and set it equal to the slider's value expressed as an Int

--- RW-Bullseye-Storyboard/ViewController.swift
+++ RW-Bullseye-Storyboard/ViewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is now: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
     
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
     }
     
-
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = Int(slider.value)
+    }
     
     
     
