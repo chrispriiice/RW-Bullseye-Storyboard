91f7fb47dc6718cc20ac3642edc3f8704362ae4f
Chris Price <christopher.r.price@gmail.com>
2020-12-10 18:24:41 -0500

    Updated the difference algorithm

--- RW-Bullseye-Storyboard/ViewController.swift
+++ RW-Bullseye-Storyboard/ViewController.swift
@@ -26,16 +26,14 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         
-        var difference: Int
-        if currentValue > targetValue {
-            difference = currentValue - targetValue
-        } else if targetValue > currentValue {
-            difference = targetValue - currentValue
-        } else {
-            difference = 0
+        var difference = currentValue - targetValue
+        if difference < 0 {
+            difference = difference * -1
         }
         
-        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        let message = "The value of the slider is now: \(currentValue)" +
+            "\nThe target value is: \(targetValue)" +
+            "\nThe difference is: \(difference)"
         
         let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
     
