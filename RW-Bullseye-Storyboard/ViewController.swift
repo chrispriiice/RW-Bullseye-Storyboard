b216ba5ccb8547a9a0d104dcbeb3902a47fa0c0d
Chris Price <christopher.r.price@gmail.com>
2020-12-10 18:21:33 -0500

    Added difference var

--- RW-Bullseye-Storyboard/ViewController.swift
+++ RW-Bullseye-Storyboard/ViewController.swift
@@ -11,13 +11,31 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
-
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = Int(slider.value.rounded())
+        targetValue = Int.random(in: 1...100)
+        startNewRound()
+        updateLabels()
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is now: \(currentValue)"
+        
+        var difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        
+        let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
     
@@ -26,14 +44,23 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabels()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now: \(slider.value)")
-        currentValue = Int(slider.value)
+        currentValue = Int(slider.value.rounded())
     }
     
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
     
-    
+    func updateLabels() {
+        targetValueLabel.text = String(targetValue)
+    }
 }
 
