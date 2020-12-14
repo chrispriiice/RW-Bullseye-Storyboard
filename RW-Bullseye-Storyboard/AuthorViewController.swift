70de13b56d345f53209fba4a77b10e1c32a2aab1
Chris Price <christopher.r.price@gmail.com>
2020-12-13 22:29:34 -0500

    Added all the image assets. Stylized the home screen except for the slider. Still needs constraints.

---
+++ RW-Bullseye-Storyboard/AuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorViewController.swift
+//  RW-Bullseye-Storyboard
+//
+//  Created by Chris Price on 12/12/20.
+//  Copyright © 2020 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
